1998e94da00a21c719bf67e1e63ea031323b345a
黄炜 <ethan@rocketai.cn>
2020-03-19 16:59:14 +0800

    update

--- config.py
+++ config.py
@@ -6,18 +6,6 @@ WX_ACCESS_TOKEN_STORE_WAY = 2
 
 #微信公众号配置
 WECHAT_CONF = {
-    #白熊规划服务号
-    'wx6fbc9936bb2b5416': {
-        "appsecret":"77dcc292643a36936f6c7fae71e7f26f",
-        "token":"RocketAi",
-        "pay": {
-            "notify_url": "http://cnzy.easy.echosite.cn/wechat_portal/pay/notify",
-            "mch_id": "1540760681",
-            "body": "1v1支付",
-            "trade_type": "JSAPI",
-        },
-        "pay_key": "a7a3407396eaa2bc940fdd66ffe5d725"
-    },
     #测试服务号
     'wxbd77bc158b32c535': {
         "appsecret":"203c9565ef21b39ecabe6698d7537546",
